30b015ba637b0e431832d969bb23f9f4c1e5915e
msurmenok <mariasurmenok@gmail.com>
2019-06-28 15:23:33 -0700

    added __eq__ function to class Dog

--- Dog.py
+++ Dog.py
@@ -19,3 +19,9 @@ class Dog:
 
     def __repr__(self):
         return "[" + str(self.id) + ", " + self.name + ", " + self.age + ", " + self.city + ", " + str(self.breed1) + ", " + str(self.breed2) + ", " + str(self.photo_large) + "]\n"
+
+    def __eq__(self, other):
+        return self.id == other.id and self.org_id == other.org_id and self.breed1 == other.breed1 \
+        and self.breed2 == other.breed2 and self.age == other.age and self.gender == other.gender \
+        and self.spayed == other.spayed and self.name == other.name and self.photo_large == other.photo_large \
+        and self.email == other.email and self.phone == other.phone and self.city == other.city
