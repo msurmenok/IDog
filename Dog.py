e0f5283d971ee0d0c13b4c57843e293e8ca94685
msurmenok <mariasurmenok@gmail.com>
2019-06-27 17:18:29 -0700

    class Dog to save information about dog from PetFinder API

---
+++ Dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    def __init__(self, id, org_id, breed1, breed2, age, gender, spayed,
+      name, photo_large, email, phone, city):
+      self.id = id
+      seif.org_id = org_id
+      self.breed1 = breed1
+      self.breed2 = breed2
+      self.age = age
+      self.gender = gender
+      self.spayed = spayed
+      self.name = name
+      self.photo_large = photo_large
+      self.email = email
+      self.phone = phone
+      self.city = city
